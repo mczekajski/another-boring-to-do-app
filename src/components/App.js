cf2ca43d37842df591c61cfe0df559035aaf5ed2
Marcin Czekajski <czekajskimarcin@gmail.com>
2020-11-20 20:23:57 +0100

    Handle adding tasks

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import AddTask from "./AddTask";
 import TaskList from "./TaskList";
 
 class App extends Component {
+  counter = 9;
   state = {
     tasks: [
       {
@@ -98,19 +99,38 @@ class App extends Component {
     tasks.forEach((task) => {
       if (task.id === id) {
         task.active = false;
-        task.finishDate = new Date().getTime()
+        task.finishDate = new Date().getTime();
       }
     });
     this.setState({
-      tasks
-    })
+      tasks,
+    });
+  };
+
+  addTask = (text, date, important) => {
+    console.log("Dodano obiekt");
+    const task = {
+      id: this.counter,
+      text, 
+      date, 
+      important,
+      active: true,
+      finishDate: null,
+    }
+    this.counter++;
+
+    this.setState(prevState => ({
+      tasks: [...prevState.tasks, task]
+    }))
+
+    return true;
   };
 
   render() {
     return (
       <div className="App">
         <h1>TO-DO APP</h1>
-        <AddTask />
+        <AddTask add={this.addTask} />
         <TaskList
           tasks={this.state.tasks}
           delete={this.deleteTask}
