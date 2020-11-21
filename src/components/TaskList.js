1056bfadd2680f718eec81b7810d9b08d1c8a44b
Marcin Czekajski <czekajskimarcin@gmail.com>
2020-11-21 10:29:38 +0100

    sort tasks

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -4,6 +4,17 @@ import Task from "./Task";
 const TaskList = (props) => {
   const active = props.tasks.filter((task) => task.active);
   const done = props.tasks.filter((task) => !task.active);
+
+  if(done.length >= 2) done.sort((a,b) => b.finishDate - a.finishDate);
+  if(active.length >= 2) active.sort((a,b) => {
+    a = a.text.toLowerCase();
+    b = b.text.toLowerCase();
+
+    if (a < b) return -1;
+    if (a > b) return 1;
+    return 0;
+  })
+
   const activeTasks = active.map((task) => (
     <Task
       key={task.id}
